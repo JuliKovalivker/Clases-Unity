80c466936d1308004dbe35a5d812d655daff17a4
JuliKovalivker <101721164+JuliKovalivker@users.noreply.github.com>
2024-09-11 23:06:42 -0300

    primera clase de unity terminada

---
+++ Clase1/Transform/Assets/cube_script.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube_script : MonoBehaviour
+{
+    public GameObject myCube;
+    private Vector3 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello Wold");
+        pos = myCube.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //var pos = new Vector3();
+        myCube.transform.position += new Vector3(0.1f, 0f, 0f);
+        Debug.Log(pos);
+        Debug.Log(Input.GetKey(KeyCode.D));
+        Debug.Log(Input.GetKey(KeyCode.DownArrow));
+        Debug.Log(Input.GetKey(KeyCode.UpArrow));
+        Debug.Log(Input.GetKeyDown(KeyCode.UpArrow));
+        Debug.Log(Input.GetKeyUp(KeyCode.UpArrow));
+        bool down_arrow = Input.GetKey(KeyCode.DownArrow);
+        Debug.Log(down_arrow);
+
+    }
+}
